85bc92b512df371f187e85b43cd3ca4167fc956a
Михаил <misha1234567890m@gmail.com>
2022-07-14 17:47:01 +0300

    Сверстал калькулятор, Использование hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
+import React, { useState } from "react";
+
 import Btn from "./components/Btn.jsx";
 
-const math = [
-  { id:"AC",
-    title:"AC"},
-    { id:"+-",
-    title:"+/-"},
-    { id:"%",
-    title:"%"},
-    { id:"7",
-    title:"7"},
-    { id:"8",
-    title:"8"},
-    { id:"9",
-    title:"9"},
-    { id:"4",
-    title:"4"},
-    { id:"5",
-    title:"5"},
-    { id:"6",
-    title:"6"},
-    { id:"1",
-    title:"1"},
-    { id:"2",
-    title:"2"},
-    { id:"3",
-    title:"3"},
-    { id:"0",
-    title:"0"},
-    { id:".",
-    title:"."},
-];
-
 function App() {
+  const [math, setMath] = useState("0");
+  const [result, setResult] = useState('')
+  
+  
+  // const [result,setResult]=useState(Number)
   return (
     <div className="wrapper">
       <div className="header">
-        <p>0</p>
-        <Btn math={math} />
+        <p>{math} </p>
+        {/* <button onClick={()=>setMath(math+1)}>asdasd</button> */}
+        <Btn 
+        math={math}
+        
+        calculator={setMath} />
       </div>
     </div>
   );
--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
-import styles from "./Btn.module.scss"
-
+import styles from "./Btn.module.scss";
 
 const Btn = (props) => {
- 
-
-
-
-  return (
-  
-  
-  
- <div className={styles.z}>
-        
-        <div className={styles.button}>
-           <button>jj</button>
-    <div className={styles.button}>
-            {props.math.map((el)=><button key={el.id}>{el.title} </button>
-                
-            )}
-        </div>
-
-        </div>
    
-    
-     </div>
-  )     
+  return (
+    <div className={styles.z}>
+      <div className={styles.button}>
+        <div className={styles.btn}>
+          <button onClick={()=>props.calculator(0)}>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button className={styles.btn1}>÷</button>
+        </div>
+        <div className={styles.btn}>
+          <button onClick={()=>props.calculator(7)}>7</button>
+          <button onClick={()=>props.calculator(8)}>8</button>
+          <button onClick={(e)=>props.calculator(props.math+e.target.innerHTML)}>9</button>
+          <button className={styles.btn1}>*</button>
+        </div>
+        <div className={styles.btn}>
+          <button onClick={()=>props.calculator(4)}>4</button>
+          <button onClick={()=>props.calculator(5)}>5</button>
+          <button onClick={()=>props.calculator(6)}>6</button>
+          <button className={styles.btn1}>-</button>
+        </div >
+        <div className={styles.btn}>
+        <button onClick={()=>props.calculator(1)}>1</button>
+        <button onClick={()=>props.calculator(2)}>2</button>
+        <button onClick={()=>props.calculator(3)}>3</button>
+        <button onClick={(e)=>{props.setMath(props.math+e.target.innerHTML)}} className={styles.btn1}>+</button>
+        </div>
+        <div className={styles.btn}>
+          <button onClick={()=>props.calculator(0)} className={styles.btn2}>0</button>
+          <button onClick={()=>props.calculator(".")}>.</button>
+          <button className={styles.btn1}>=</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Btn;
