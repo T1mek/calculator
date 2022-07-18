7a78b2e80b87fd3c8b71551b0433e4dad59c154f
Михаил <misha1234567890m@gmail.com>
2022-07-18 18:06:02 +0300

    Рефакторинг + получение результата

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import Btn from "./components/Btn.jsx";
 
 function App() {
   const [math, setMath] = useState("0");
-  const [result, setResult] = useState('')
-  
-  
+  const [result, setResult] = useState("");
+
+  function applyExpression(countedNumber) {
+    setMath(countedNumber);
+    // eslint-disable-next-line no-eval
+    setResult(eval(math));
+  }
+
   // const [result,setResult]=useState(Number)
   return (
     <div className="wrapper">
       <div className="header">
-        <p>{math} </p>
+        <p>
+          {math} <span>{result}</span>
+        </p>
         {/* <button onClick={()=>setMath(math+1)}>asdasd</button> */}
-        <Btn 
-        math={math}
-        
-        calculator={setMath} />
+        <Btn
+          math={math}
+          calculator={setMath}
+          expression={"/"}
+          onClick={applyExpression}
+          equal={result}
+        />
       </div>
     </div>
   );
--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,38 +2,46 @@ import React, { useState } from "react";
 import styles from "./Btn.module.scss";
 
 const Btn = (props) => {
+
+function numEnt(e){
+if(props.math !=="0"){props.calculator(props.math+e.target.innerHTML)}
+else props.calculator(e.target.innerHTML)
+}
+
+
+
    
   return (
     <div className={styles.z}>
       <div className={styles.button}>
         <div className={styles.btn}>
-          <button onClick={()=>props.calculator(0)}>AC</button>
+          <button onClick={()=>props.calculator("")}>AC</button>
           <button>+/-</button>
-          <button>%</button>
-          <button className={styles.btn1}>÷</button>
+          <button onClick={(e)=>props.calculator(props.math+e.target.innerHTML)}>%</button>
+          <button onClick={numEnt} className={styles.btn1}>/</button>
         </div>
         <div className={styles.btn}>
-          <button onClick={()=>props.calculator(7)}>7</button>
-          <button onClick={()=>props.calculator(8)}>8</button>
-          <button onClick={(e)=>props.calculator(props.math+e.target.innerHTML)}>9</button>
-          <button className={styles.btn1}>*</button>
+          <button onClick={numEnt}>7</button>
+          <button onClick={numEnt}>8</button>
+          <button onClick={numEnt}>9</button>
+          <button onClick={numEnt} className={styles.btn1}>*</button>
         </div>
         <div className={styles.btn}>
-          <button onClick={()=>props.calculator(4)}>4</button>
-          <button onClick={()=>props.calculator(5)}>5</button>
-          <button onClick={()=>props.calculator(6)}>6</button>
-          <button className={styles.btn1}>-</button>
+          <button onClick={numEnt}>4</button>
+          <button onClick={numEnt}>5</button>
+          <button onClick={numEnt}>6</button>
+          <button onClick={numEnt} className={styles.btn1}>-</button>
         </div >
         <div className={styles.btn}>
-        <button onClick={()=>props.calculator(1)}>1</button>
-        <button onClick={()=>props.calculator(2)}>2</button>
-        <button onClick={()=>props.calculator(3)}>3</button>
-        <button onClick={(e)=>{props.setMath(props.math+e.target.innerHTML)}} className={styles.btn1}>+</button>
+        <button onClick={numEnt}>1</button>
+        <button onClick={numEnt}>2</button>
+        <button onClick={numEnt}>3</button>
+        <button onClick={numEnt} className={styles.btn1}>+ </button>
         </div>
         <div className={styles.btn}>
-          <button onClick={()=>props.calculator(0)} className={styles.btn2}>0</button>
-          <button onClick={()=>props.calculator(".")}>.</button>
-          <button className={styles.btn1}>=</button>
+          <button onClick={numEnt} className={styles.btn2}>0</button>
+          <button onClick={numEnt}>.</button>
+          <button onClick={()=>props.onClick()} className={styles.btn1}>=</button>
         </div>
       </div>
     </div>
